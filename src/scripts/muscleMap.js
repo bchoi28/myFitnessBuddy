a46d049dcaf14114835c21c89bf47f78b19ff082
Brandon Choi <bchoi28@gmail.com>
2023-08-09 23:46:27 -0400

    Add back view

--- src/scripts/muscleMap.js
+++ src/scripts/muscleMap.js
@@ -6,6 +6,7 @@ class MuscleMap {
         this.myCarousel = myCarousel;
         this.render();
         this.renderBack();
+        this.renderToggleButton();
         this.handleClickGenerateExercise = this.handleClickGenerateExercise.bind(this);
         this.bindEvents();
     };
@@ -27,6 +28,9 @@ class MuscleMap {
         const rightChest = document.createElement('div');
         rightChest.classList.add('right-chest');
         chest.appendChild(rightChest);
+
+        const back = document.createElement('div');
+        back.classList.add('back');
 
         const leftShoulder = document.createElement('div');
         leftShoulder.classList.add('left-shoulder');
@@ -60,8 +64,8 @@ class MuscleMap {
             absContainer.appendChild(abs);
         };
 
-        // const leftGlute = document.createElement('div');
-        // leftGlute.classList.add('left-glute');
+        const leftGlute = document.createElement('div');
+        leftGlute.classList.add('left-glute');
 
         const leftQuad = document.createElement('div');
         leftQuad.classList.add('left-quad');
@@ -72,8 +76,8 @@ class MuscleMap {
         const leftFoot = document.createElement('div');
         leftFoot.classList.add('left-foot');
 
-        // const rightGlute = document.createElement('div');
-        // rightGlute.classList.add('right-glute');
+        const rightGlute = document.createElement('div');
+        rightGlute.classList.add('right-glute');
 
         const rightQuad = document.createElement('div');
         rightQuad.classList.add('right-quad');
@@ -86,8 +90,8 @@ class MuscleMap {
 
         muscleMap.appendChild(head);
         muscleMap.appendChild(chest);
-        // muscleMap.appendChild(leftChest);
-        // muscleMap.appendChild(rightChest);
+        muscleMap.appendChild(back);
+
         muscleMap.appendChild(leftShoulder);
         muscleMap.appendChild(leftBicep);
         muscleMap.appendChild(leftForearm);
@@ -97,32 +101,95 @@ class MuscleMap {
         muscleMap.appendChild(rightBicep);
         muscleMap.appendChild(rightHand);
         muscleMap.appendChild(absContainer);
-        // muscleMap.appendChild(leftGlute);
+        muscleMap.appendChild(leftGlute);
         muscleMap.appendChild(leftQuad);
         muscleMap.appendChild(leftCalf);
         muscleMap.appendChild(leftFoot);
-        // muscleMap.appendChild(rightGlute);
+        muscleMap.appendChild(rightGlute);
         muscleMap.appendChild(rightQuad);
         muscleMap.appendChild(rightCalf);
         muscleMap.appendChild(rightFoot);
 
         this.container.appendChild(muscleMap);
-
-        const toggleButton = document.createElement('button');
-        toggleButton.id = 'toggle-view';
-        toggleButton.innerText = "Flip";
-        this.container.appendChild(toggleButton);
-
     };
 
 
     renderBack() {
         const muscleMapBack = document.createElement('div');
         muscleMapBack.classList.add('muscle-map-back');
-        muscleMapBack.innerText = 'Hi';
 
+        const head2 = document.createElement('div');
+        head2.classList.add('head-2');
 
-        // Define and append back body parts here, just like you did for the front
+        const back2 = document.createElement('div');
+        back2.classList.add('back-2');
+
+        const leftShoulder = document.createElement('div');
+        leftShoulder.classList.add('left-shoulder');
+
+        const leftBicep = document.createElement('div');
+        leftBicep.classList.add('left-bicep');
+
+        const leftForearm = document.createElement('div');
+        leftForearm.classList.add('left-forearm');
+
+        const leftHand = document.createElement('div');
+        leftHand.classList.add('left-hand');
+
+        const rightShoulder = document.createElement('div');
+        rightShoulder.classList.add('right-shoulder');
+
+        const rightBicep = document.createElement('div');
+        rightBicep.classList.add('right-bicep');
+
+        const rightForearm = document.createElement('div');
+        rightForearm.classList.add('right-forearm');
+
+        const rightHand = document.createElement('div');
+        rightHand.classList.add('right-hand');
+
+        const leftGlute2 = document.createElement('div');
+        leftGlute2.classList.add('left-glute-2');
+
+        const rightGlute2 = document.createElement('div');
+        rightGlute2.classList.add('right-glute-2');
+
+        const leftQuad = document.createElement('div');
+        leftQuad.classList.add('left-quad');
+
+        const leftCalf = document.createElement('div');
+        leftCalf.classList.add('left-calf');
+
+        const leftFoot = document.createElement('div');
+        leftFoot.classList.add('left-foot');
+
+        const rightQuad = document.createElement('div');
+        rightQuad.classList.add('right-quad');
+
+        const rightCalf = document.createElement('div');
+        rightCalf.classList.add('right-calf');
+
+        const rightFoot = document.createElement('div');
+        rightFoot.classList.add('right-foot');
+
+        muscleMapBack.appendChild(head2);
+        muscleMapBack.appendChild(back2);
+        muscleMapBack.appendChild(leftShoulder);
+        muscleMapBack.appendChild(leftBicep);
+        muscleMapBack.appendChild(leftForearm);
+        muscleMapBack.appendChild(leftHand);
+        muscleMapBack.appendChild(rightShoulder);
+        muscleMapBack.appendChild(rightForearm);
+        muscleMapBack.appendChild(rightBicep);
+        muscleMapBack.appendChild(rightHand);
+        muscleMapBack.appendChild(leftGlute2);
+        muscleMapBack.appendChild(leftQuad);
+        muscleMapBack.appendChild(leftCalf);
+        muscleMapBack.appendChild(leftFoot);
+        muscleMapBack.appendChild(rightGlute2);
+        muscleMapBack.appendChild(rightQuad);
+        muscleMapBack.appendChild(rightCalf);
+        muscleMapBack.appendChild(rightFoot);
 
         this.container.appendChild(muscleMapBack);
     }
@@ -133,7 +200,7 @@ class MuscleMap {
 
         if (frontView.style.display !== 'none') {
             frontView.style.display = 'none';
-            backView.style.display = 'block';
+            backView.style.display = 'grid';
         } else {
             frontView.style.display = 'grid';
             backView.style.display = 'none';
@@ -145,6 +212,13 @@ class MuscleMap {
 
         const toggleButton = document.getElementById('toggle-view');
         toggleButton.addEventListener('click', this.toggleView.bind(this));
+    }
+
+    renderToggleButton() {
+        const toggleButton = document.createElement('button');
+        toggleButton.id = 'toggle-view';
+        toggleButton.innerText = "Flip";
+        this.container.appendChild(toggleButton);
     }
 
     // addEventListeners() {
